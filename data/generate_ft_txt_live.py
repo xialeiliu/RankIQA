6e7deacc5e63036a1d210c2d6d0e1431308f92d6
Xialei Liu <xialei@cvc.uab.es>
2017-08-02 20:49:39 +0200

    Added functions for live dataset

--- data/generate_ft_txt_live.py
+++ data/generate_ft_txt_live.py
@@ -1,54 +1,80 @@
-
 import numpy as np
 import scipy.io as sio
 import os
 import os.path as osp
+import cv2
 
-data_dir = 'data/tid2013/'
-
-file_root = data_dir + 'distorted_images' + '/'
-list_file = data_dir + 'mos_with_names' + '.txt'
+   
+label = 'LIVE2_labels.mat'
+shape = ['gblur','wn','jpeg','jp2k','fastfading']
+data_path = 'data/tid2013/' 
+list_file = 'ref' + '.txt'
 
 
 filename = [line.rstrip('\n') for line in open(
-            osp.join(list_file))]
-S_name = [] 
-scores = []
-for i in filename:
-    S_name.append(i.split()[1])
-    scores.append(float(i.split()[0]))
+            osp.join(data_path, list_file))]
             
-ref = S_name
+ref = filename
+            
+gt_label = sio.loadmat(label)
 
-TotalNum = len(ref)
-shuff = np.random.permutation(range(1,25))
+shuff = np.random.permutation(range(29))
+train_file = open('ft_live_train.txt', "w")
+test_file = open('ft_live_test.txt', "w")
 
-Num_tr = 19  # ~80% of reference images as training samples
-Num_te = 5   # ~20% of reference images as testing samples
+Num_tr = 23
+Num_te = 29
+test = False
+flag = 1
 
-train_file = open('ft_tid2013'+'_train.txt', "w")
-test_file = open('ft_tid2013'+'_test.txt', "w")
-
-shuff_txt=np.random.permutation(range(TotalNum))    
+for tp in shape:
     
-for i in shuff_txt:
-        #if ref[i][4:6]==tp:
-        for j in range(Num_tr):
-            if int(ref[i][1:3]) == shuff[j]:
-               folder = data_dir + 'distorted_images/' + ref[i]
-               labels = scores[i]
-               train_file.write('%s %6.2f\n' % (folder,labels))
-
-
+    file_root = data_path + tp + '/'
+    list_file = 'info' + '.txt'
+    filename = [line.rstrip('\n') for line in open(
+            osp.join(file_root, list_file))]
+    S_name = [] 
+    N_name = []
+    scores = []
+    for i in filename:
+        S_name.append(i.split()[0])
+        N_name.append(i.split()[1])
+        scores.append(float(i.split()[2]))
+        temp_label = gt_label[tp]
+        temp_label = temp_label.swapaxes(1,0)
     
+    a = os.listdir(tp)
+    a.sort()
+    
+    TotalNum = temp_label.shape[0]
 
-for i in shuff_txt:
-        #if ref[i][4:6]==tp:
-        for j in range(Num_tr,Num_tr+Num_te):
-            if int(ref[i][1:3]) == shuff[j]:
-               folder = data_dir + 'distorted_images/' + ref[i]
-               labels = scores[i]
+    for i in range(Num_tr):
+        for j in range(TotalNum):
+            if S_name[j] == ref[shuff[i]]:
+               folder = data_path + tp + '/' + a[int(N_name[j][3:-4])-1] 
+               labels = temp_label[int(N_name[j][3:-4])-1] 
+               train_file.write('%s %6.2f\n' % (folder,labels))   
+    
+    
+    for i in range(Num_tr,Num_te):
+        for j in range(TotalNum):
+            if S_name[j] == ref[shuff[i]]:
+               folder = data_path+ tp + '/' + a[int(N_name[j][3:-4])-1] 
+               if test  ==True:
+                   tmp  = cv2.imread(folder)
+                   cv2.imwrite('./test/' + str(flag)+'.bmp',tmp)
+                   flag += 1
+
+               labels = temp_label[int(N_name[j][3:-4])-1] 
                test_file.write('%s %6.2f\n' % (folder,labels))
 
-train_file.close()   
+
+train_file.close()
 test_file.close()
+
+
+
+
+
+
+
