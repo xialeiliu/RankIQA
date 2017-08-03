287f0f2804c088d43398beea5eca10d4377ad23a
Xialei Liu <xialei@cvc.uab.es>
2017-08-03 15:40:09 +0200

    Correct the function of rename

--- data/rename_image_names.py
+++ data/rename_image_names.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import os.path as osp
 import cv2
+import pdb
 
 shape = ['gblur','wn','jpeg','jp2k','fastfading']
 data = './live/' 
@@ -13,27 +14,22 @@ for tp in shape:
     list_file = 'info' + '.txt'
     filename = [line.rstrip('\n') for line in open(
             osp.join(file_root, list_file))]
-    S_name = [] 
     N_name = []
-    scores = []
     for i in filename:
-        S_name.append(i.split()[0])
         N_name.append(i.split()[1])
-        scores.append(float(i.split()[2]))
     
-    
+    pdb.set_trace()
     
     for j in range(len(N_name)):
         folder = data +tp +   '/' + N_name[j]
 
         tmp  = cv2.imread(folder)
-    
-
         cv2.imwrite(data+ tp + '/' + 'img' + str(int(N_name[j][3:-4])).zfill(3)+'.bmp',tmp)
 
 
         if int(N_name[j][3:-4])<100:            
             os.remove(folder)
+    os.remove(data+tp+'/'+'Thumbs.db')
 
 
 
