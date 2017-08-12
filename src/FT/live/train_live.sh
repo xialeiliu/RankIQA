26f64fc7cda2b76f7cd7cb479557bffe7d270ff3
Xialei Liu <xialei@cvc.uab.es>
2017-08-12 19:26:03 +0200

    Update

--- src/FT/live/train_live.sh
+++ src/FT/live/train_live.sh
@@ -2,4 +2,4 @@
 
 TOOLS=/home/xialei/caffe/distribute/bin #TODO Changing to your Caffe bin
 
-$TOOLS/caffe.bin train --gpu 2 --solver=src/FT/live/solver_live.prototxt --weights ./models/rank_live/my_siamese_iter_50000.caffemodel  2>&1 | tee  models/ft_live/log_live
+$TOOLS/caffe.bin train --gpu 0 --solver=src/FT/live/solver_live.prototxt --weights ./models/rank_live_20/my_siamese_iter_20000.caffemodel  2>&1 | tee  models/ft_live/log_live
